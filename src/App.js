6f976a40426a6838440ee9cfab963912c97f134b
Tomi Mandala Putra <tommymandalaputra@gmail.com>
2023-07-08 12:35:26 +0700

    feat: create style app.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Outlet } from "react-router-dom";
+import "./assets/css/app.css";
 
 function App() {
-  return <Outlet />;
+  return (
+    <main className="app__main">
+      <Outlet />
+    </main>
+  );
 }
 
 export default App;

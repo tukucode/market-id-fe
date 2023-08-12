e0d56b9b505078eb872d4db800042e82e12a7122
Tomi Mandala Putra <tommymandalaputra@gmail.com>
2023-08-12 21:56:15 +0700

    feat: handle admin

--- src/Layouts/LayoutAuth.jsx
+++ src/Layouts/LayoutAuth.jsx
@@ -1,9 +1,17 @@
 import { Navigate, useLocation } from "react-router-dom";
 
 const LayoutAuth = ({ auth, children }) => {
-  const { token } = auth;
+  const { token, user } = auth;
   const location = useLocation();
   const isPage = ["/login", "/register"].includes(location.pathname);
+
+  // role admin
+  if (user.role && ['admin'].includes(user.role.name)) {
+    localStorage.removeItem('user')
+    localStorage.removeItem('token')
+    if (isPage) return children;
+    return <Navigate to="/login" replace />;
+  }
 
   if (!token) {
     if (isPage) return children;

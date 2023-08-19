de11715d16a255003922fc59ad002457921e16ea
Tomi Mandala Putra <tommymandalaputra@gmail.com>
2023-08-19 08:02:11 +0700

    fix: setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const proxyBaseUrl = {
-  target: ['production'].includes(process.env.NODE_ENV) ? process.env.REACT_APP_API_BASE_URL : process.env.REACT_APP_API_BASE_URL,
+  target: process.env.REACT_APP_API_BASE_URL || '',
   changeOrigin: true,
   pathRewrite: {
     "^/api": "",
@@ -9,7 +9,7 @@ const proxyBaseUrl = {
 }
 
 const proxyWilayah = {
-  target:  ['production'].includes(process.env.NODE_ENV) ? process.env.REACT_APP_API_WILAYAH : process.env.REACT_APP_API_WILAYAH,
+  target: process.env.REACT_APP_API_WILAYAH || '',
   changeOrigin: true,
   pathRewrite: {
     "^/api-wilayah": "",
